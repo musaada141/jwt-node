cfa2d600debd7c89c1cff9d109594d9ba4601937
musaada141 <94170521+musaada141@users.noreply.github.com>
2023-06-09 06:29:32 +0100

    second commit

--- controller/loginController.js
+++ controller/loginController.js
@@ -4,14 +4,23 @@ const loginController = {
   login: async (req, res) => {
     let userDetails = {
       username: "musa ada",
-      password: "musaadg001/",
+      password: "12345",
     };
 
-    if (userDetails.username === req.params.username) {
+    if (userDetails.password === req.params.password) {
       jwt.sign(userDetails, process.env.SECRET_KEY, (err, data) => {
-        if (err) throw err;
-        return data;
+        if (err) {
+          res.json("error");
+        }
+        return res.json({
+          token: data,
+          msg: `welcome ${userDetails.username}`,
+        });
       });
+    } else {
+      return res.json({ msg: `Invalid Password` });
     }
   },
 };
+
+module.exports = loginController;
